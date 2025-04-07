c9025dcae3396f1050f2afa3c05e1675c979dcdb
Ohgiraffers-bear <bear.ohgiraffers@gmail.com>
2025-04-07 17:14:59 +0900

    deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // axios 인스턴스 생성
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://kb-json.onrender.com/",
   timeout: 5000, // 5초
 });
 
